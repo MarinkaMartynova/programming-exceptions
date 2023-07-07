4510161cf969dc8c31d3afefee6412169bd731a8
MarinkaMartynova <saramsok@gmail.com>
2023-07-08 00:10:26 +0400

    add homework 3

---
+++ Homework_3/src/DataRequest.java
@@ -0,0 +1,30 @@
+
+import java.io.FileWriter;
+import java.util.Scanner;
+
+// Запрос данных
+public class DataRequest {
+    //запись данных
+    public static void dataEntry() {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Введите данные пользователя в формате: " +
+                "Фамилия Имя Отчество датарождения номертелефона пол\n" +
+                "Форматы данных:\n" +
+                "- фамилия, имя, отчество - строки\n" +
+                "- датарождения - строка формата dd.mm.yyyy\n" +
+                "- номертелефона - строка в виде семизначного числа\n" +
+                "- пол - символ латиницей f или m.\n" +
+                "- Или введите exit для выхода\n");
+
+        boolean run = true;
+        while (run) {
+            String[] arr = in.nextLine().split(" ");
+            if (arr[0].equalsIgnoreCase("exit")) {
+                break;
+            }
+            DataValidator.arrayEnteredData(arr);
+        }
+        System.out.println("Выход из приложения");
+    }
+}
